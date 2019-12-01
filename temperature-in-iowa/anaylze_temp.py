7143462a84c2d213c12b13630e09c522ad903c14
jack <jackmead515@gmail.com>
2019-11-30 20:45:57 -0600

    adding future predicitions

--- temperature-in-iowa/anaylze_temp.py
+++ temperature-in-iowa/anaylze_temp.py
@@ -11,20 +11,43 @@ from keras import optimizers
 from keras import losses
 from datetime import datetime
 
-def load_data():
-  df = pd.read_csv('ames_iowa_30-09-1996_26-10-2019_temp.csv')
+def load_temp_humid_data():
+  df = pd.read_csv('ames_iowa_30-09-1996_26-10-2019_temp_humid.csv')
   df = df.drop(['station'], axis=1)
   df = df.dropna()
-  df.columns = ['time', 'value']
+  df.columns = ['time', 'temp', 'humid']
   # df.time = pd.DatetimeIndex(df.time).astype(np.int64)
   df.time = [datetime.strptime(x, "%Y-%m-%d %H:%M").timestamp() for x in df.time]
   return df
 
-def forecast(data, window_size):
+def load_temp_data():
+  df = pd.read_csv('ames_iowa_30-09-1996_26-10-2019_temp.csv')
+  df = df.drop(['station'], axis=1)
+  df = df.dropna()
+  df.columns = ['time', 'temp']
+  # df.time = pd.DatetimeIndex(df.time).astype(np.int64)
+  df.time = [datetime.strptime(x, "%Y-%m-%d %H:%M").timestamp() for x in df.time]
+  return df
+
+def forecast_temp(data, window_size, future_window):
   datax, datay = [], []
-  for i in range(len(data)-window_size-1):
+  for i in range(len(data)-window_size-future_window-1):
     datax.append(data[i:(i+window_size), 1])
-    datay.append(data[i + window_size, 1])
+    datay.append([])
+    for x in range(future_window):
+      datay[i].append(data[i + x + window_size, 1])
+  return datax, datay
+
+def forecast_temp_humid(data, window_size, future_window):
+  datax, datay = [], []
+  for i in range(len(data)-window_size-future_window-1):
+    datax.append([
+      data[i:(i+window_size), 1],
+      data[i:(i+window_size), 2]
+    ])
+    datay.append([])
+    for x in range(future_window):
+      datay[i].append(data[i + x + window_size, 1])
   return datax, datay
 
 def split_years(df):
@@ -34,10 +57,9 @@ def split_years(df):
   current_year = datetime.fromtimestamp(values[0, 0]).year
   for pair in values:
     time = pair[0]
-    temp = pair[1]
     data_year = datetime.fromtimestamp(time).year
     if data_year == current_year:
-      years[index].append(np.array([time, temp]))
+      years[index].append(np.array(pair))
     else:
       index += 1
       current_year = data_year
@@ -58,6 +80,14 @@ def graph_years(df):
     plot_index+=1
   plot.show()
 
+def graph_year(df):
+  years = np.array(split_years(df))
+  year = np.array(years[5])
+  figure = plot.figure(facecolor='white')
+  plot.plot(year[:, 0], year[:, 1], color=[0.0, 1.0, 0.0])
+  plot.plot(year[:, 0], year[:, 2], color=[0.0, 0.0, 1.0])
+  plot.show()
+
 def split_months(year):
   values = np.array(year)
   months = [[]]
@@ -65,10 +95,9 @@ def split_months(year):
   current_month = datetime.fromtimestamp(values[0, 0]).month
   for pair in values:
     time = pair[0]
-    temp = pair[1]
     data_month = datetime.fromtimestamp(time).month
     if data_month == current_month:
-      months[index].append(np.array([time, temp]))
+      months[index].append(np.array(pair))
     else:
       index += 1
       current_month = data_month
@@ -82,10 +111,9 @@ def split_days(month):
   current_day = datetime.fromtimestamp(values[0, 0]).day
   for pair in values:
     time = pair[0]
-    temp = pair[1]
     data_day = datetime.fromtimestamp(time).day
     if data_day == current_day:
-      days[index].append(np.array([time, temp]))
+      days[index].append(np.array(pair))
     else:
       index += 1
       current_day = data_day
@@ -144,32 +172,36 @@ def points_per_month(df):
   print('Median: ' + str(np.median(lengths)))
   print('Mode: ' + str(np.average(lengths)))
 
-def format_traintest_data(df, window_size, train_test_split):
+def format_traintest_data(df, window_size, future_window, train_test_split):
   scaler = MinMaxScaler(feature_range=(-1, 1))
   dataset = np.array(df)
   dataset = scaler.fit_transform(dataset)
-  datax, datay = forecast(dataset, window_size)
+  datax, datay = forecast_temp(dataset, window_size, future_window)
   tts = int(len(datax)*train_test_split)
-  x_train = np.expand_dims(np.array(datax[:tts]).astype(np.float32), axis=2)
-  y_train = np.expand_dims(np.array(datay[:tts]).astype(np.float32), axis=2)
-  x_test = np.expand_dims(np.array(datax[tts+1:]).astype(np.float32), axis=2)
-  y_test = np.expand_dims(np.array(datay[tts+1:]).astype(np.float32), axis=2)
+  x_train = np.array(datax[:tts]).astype(np.float32)
+  y_train = np.array(datay[:tts]).astype(np.float32)
+  x_test = np.array(datax[tts+1:]).astype(np.float32)
+  y_test = np.array(datay[tts+1:]).astype(np.float32)
+  x_train = np.expand_dims(x_train, axis=2)
+  y_train = np.expand_dims(y_train, axis=2)
+  x_test = np.expand_dims(x_test, axis=2)
+  y_test = np.expand_dims(y_test, axis=2)
   return x_train, y_train, x_test, y_test, scaler
 
-def format_validation_data(df, window_size):
+def format_validation_data(df, window_size, future_window):
   scaler = MinMaxScaler(feature_range=(-1, 1))
   dataset = np.array(df)
   dataset = scaler.fit_transform(dataset)
-  datax, datay = forecast(dataset, window_size)
+  datax, datay = forecast(dataset, window_size, future_window)
   x_test = np.expand_dims(np.array(datax).astype(np.float32), axis=2)
   y_test = np.expand_dims(np.array(datay).astype(np.float32), axis=2)
   return x_test, y_test, scaler
 
-def build_network(nodes, window_size, features, optimizer, loss):
+def build_network(nodes, window_size, future_window, features, optimizer, loss):
   model = models.Sequential()
   #dropout=0.2, recurrent_dropout=0.2
   model.add(layers.LSTM(nodes, input_shape=(window_size, features)))
-  model.add(layers.Dense(1))
+  model.add(layers.Dense(future_window))
   model.compile(optimizer=optimizer, loss=loss)
   return model
 
@@ -178,8 +210,8 @@ def train_network(model, x_train, y_train, epochs, batch_size):
   history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
   end_time = time.time()
   print('Training Time: ' + str(end_time-start_time) + ' secs')
-  # plot.plot(np.arange(epochs), history.history['loss'])
-  # plot.show()
+  plot.plot(np.arange(epochs), history.history['loss'])
+  plot.show()
 
 def evaluate_network(model, x_test, y_test, batch_size):
   score = model.evaluate(x_test, y_test)
@@ -195,42 +227,18 @@ def test_network(model, x_test, y_test, scaler):
   score = np.sqrt(mean_squared_error(pred_test, y_test))
   print("Testing Score (RMSE): {}".format(score))
 
-  # plot.plot(np.arange(pred_test.shape[0]), pred_test[:, 0])
+  plot.plot(np.arange(pred_test.shape[1]), pred_test[0, :], color=[0.0,0.0,1.0], marker='.')
   # plot.plot(np.arange(y_test.shape[0]), y_test[:, 0])
-  # plot.show()
-
-def gather_future_predictions(model, test, window_size, future_index):
-  preds = []
-  sequence = [test[i, 0] for i in range(window_size)]
-  for _ in range(future_index):
-    pd = model.predict(np.array([
-      np.expand_dims(np.array(sequence), axis=1)
-    ]))[0, 0]
-    preds.append(pd)
-    sequence.append(pd)
-    sequence.pop(0)
-  return preds
-
-def predict_into_future(model, test, window_size, scaler):
-  preds = gather_future_predictions(model, test, window_size, 24)
-  
-  tscaler = MinMaxScaler()
-  tscaler.min_, tscaler.scale_ = scaler.min_[1], scaler.scale_[1]
-
-  test = tscaler.inverse_transform(test)
-  plot.plot(np.arange(test.shape[0]-window_size), test[window_size:, 0], color=[1.0, 0.0, 0.0])
-
-  preds = np.expand_dims(np.array(preds), axis=1)
-  preds = tscaler.inverse_transform(preds)
-  plot.plot(np.arange(preds.shape[0]), preds[:, 0], color=[0.0, 0.0, 1.0])
-  
+  plot.plot(np.arange(48), y_test[:48, 0], color=[1.0,0.0,0.0], marker='.')
   plot.show()
 
+# https://machinelearningmastery.com/multi-step-time-series-forecasting-long-short-term-memory-networks-python
+# https://machinelearningmastery.com/convert-time-series-supervised-learning-problem-python
 ###################################################################################
 # Format
 
 print('Loading data...')
-df = load_data()
+df = load_temp_data()
 
 print('Total Data Points: {}'.format(len(df.time)))
 
@@ -239,6 +247,7 @@ print('Total Data Points: {}'.format(len(df.time)))
 
 # graph_whole(df)
 # graph_years(df)
+# graph_year(df)
 # graph_months(df)
 # graph_days(df)
 # points_per_month(df)
@@ -247,27 +256,34 @@ print('Total Data Points: {}'.format(len(df.time)))
 # Format and Standardize
 
 FEATURES = 1
-WINDOW_SIZE = 12
-TRAIN_TEST_SPLIT = 1
-NODES = 8
-EPOCHS = 30
+WINDOW_SIZE = 48
+FUTURE_WINDOW = 24
+TRAIN_TEST_SPLIT = 0.8
+NODES = 16
+EPOCHS = 100
 BATCH_SIZE = 500
 OPTIMIZER = optimizers.Adam()
 LOSS = losses.mean_squared_error
 
 years = split_years(df)
 months = split_months(years[9])
-x_train, y_train, x_test, y_test, tscaler = format_traintest_data(years[9], WINDOW_SIZE, TRAIN_TEST_SPLIT)
-x_valid, y_valid, vscaler = format_validation_data(years[10], WINDOW_SIZE)
+x_train, y_train, x_test, y_test, tscaler = format_traintest_data(years[9], WINDOW_SIZE, FUTURE_WINDOW, TRAIN_TEST_SPLIT)
 
-# https://machinelearningmastery.com/multi-step-time-series-forecasting-long-short-term-memory-networks-python
-# https://machinelearningmastery.com/convert-time-series-supervised-learning-problem-python
+# x_train = x_train.reshape((x_train.shape[0], x_train.shape[2], x_train.shape[1]))
+# x_test = x_test.reshape((x_test.shape[0], x_test.shape[2], x_test.shape[1]))
+y_test = np.squeeze(y_test, axis=2)
+y_train = np.squeeze(y_train, axis=2)
+
+print('x_train', x_train.shape)
+print('y_train', y_train.shape)
+
+#x_valid, y_valid, vscaler = format_validation_data(years[10], WINDOW_SIZE)
 
 ###################################################################################
 # Model
 
 print('Running network...')
-model = build_network(NODES, WINDOW_SIZE, FEATURES, OPTIMIZER, LOSS)
+model = build_network(NODES, WINDOW_SIZE, FUTURE_WINDOW, FEATURES, OPTIMIZER, LOSS)
 train_network(model, x_train, y_train, EPOCHS, BATCH_SIZE)
-#test_network(model, x_valid, y_valid, vscaler)
-predict_into_future(model, y_valid, WINDOW_SIZE, vscaler)
+test_network(model, x_test, y_test, tscaler)
+#predict_into_future(model, y_valid, WINDOW_SIZE, vscaler)
